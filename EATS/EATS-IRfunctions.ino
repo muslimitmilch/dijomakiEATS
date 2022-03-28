e81fe288f2e22248756c60554969e1f4c91829b1
muslimitmilch <97620732+muslimitmilch@users.noreply.github.com>
2022-03-28 19:32:19 +0200

    Update EATS-IRfunctions.ino

--- EATS/EATS-IRfunctions.ino
+++ EATS/EATS-IRfunctions.ino
@@ -44,3 +44,59 @@ int IRsignal_ON[] = {
   938, 226,
   60, 0};
 const int NumIRsignals_ON = 80; //
+
+void sendIRbett(int IRsignal, int NumIRsignals){ //rewrite; code by rick osgood
+  delay(10);
+  for (int i = 0; i < NumIRsignals; i+=2) {         //Loop through all of the IR timings
+      pulseIRmitte(IRsignal[i]*10);              //Flash IR LED at 38khz for the right amount of time
+      delayMicroseconds(IRsignal[i+1]*10);  //Then turn it off for the right amount of time
+    }
+}
+
+
+//from here on old code
+
+void signal1mitte(){ //code von rick osgood
+  delay(10);
+  for (int i = 0; i < NumIRsignals; i+=2) {         //Loop through all of the IR timings
+      pulseIRmitte(IRsignal[i]*10);              //Flash IR LED at 38khz for the right amount of time
+      delayMicroseconds(IRsignal[i+1]*10);  //Then turn it off for the right amount of time
+    }
+  }
+
+  
+void signal1bett(){ //code von rick osgood
+  delay(10);
+  for (int i = 0; i < NumIRsignals; i+=2) {         //Loop through all of the IR timings
+      pulseIRbett(IRsignal[i]*10);              //Flash IR LED at 38khz for the right amount of time
+      delayMicroseconds(IRsignal[i+1]*10);  //Then turn it off for the right amount of time
+    }
+  }
+
+
+void pulseIRmitte(long microsecs) { //code von rick osgood
+  while (microsecs > 0) {
+    // 38 kHz is about 13 microseconds high and 13 microseconds low
+   digitalWrite(IRledPinMitte, HIGH);  // this takes about 3 microseconds to happen
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   digitalWrite(IRledPinMitte, LOW);   // this also takes about 3 microseconds
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+ 
+   // so 26 microseconds altogether
+   microsecs -= 26;
+  }
+}
+
+
+void pulseIRbett(long microsecs) { //code von rick osgood
+  while (microsecs > 0) {
+    // 38 kHz is about 13 microseconds high and 13 microseconds low
+   digitalWrite(IRledPinBett, HIGH);  // this takes about 3 microseconds to happen
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   digitalWrite(IRledPinBett, LOW);   // this also takes about 3 microseconds
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+ 
+   // so 26 microseconds altogether
+   microsecs -= 26;
+  }
+}
